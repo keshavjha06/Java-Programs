3724f788bc5d426d379754d8e310ff709e0a422c
Keshav Jha <keshavjha0610@gmail.com>
2021-03-24 18:04:52 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,36 @@
+package javacoreprograms;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//using string concatenation operator
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first string");
+		String x = sc.nextLine();
+		System.out.println("Enter the second string");
+		String y = sc.nextLine();
+		x = x.replace(" ", "");
+		y = y.replace(" ", "");
+
+		x = x.toLowerCase();
+		y = y.toLowerCase();
+
+		char a[] = x.toCharArray();
+		char b[] = y.toCharArray();
+
+		Arrays.sort(a);
+		Arrays.sort(b);
+		boolean flag = Arrays.equals(a, b);
+		if (flag == true) {
+			System.out.println("String are anagram");
+		} else {
+			System.out.println("String are not anagram");
+		}
+
+	}
+
+}
