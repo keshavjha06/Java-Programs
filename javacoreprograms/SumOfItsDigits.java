63ac7b071883f4773418ace00bc41f5b6a2d2cc5
Keshav Jha <keshavjha0610@gmail.com>
2021-04-01 10:55:59 +0530

    Update SumOfItsDigits.java

--- javacoreprograms/SumOfItsDigits.java
+++ javacoreprograms/SumOfItsDigits.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SumOfItsDigits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number");
 		int num = sc.nextInt();
