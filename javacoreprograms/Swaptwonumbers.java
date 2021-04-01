4f3dedef8216ae25dd65c964095b137a5fef1bc7
Keshav Jha <keshavjha0610@gmail.com>
2021-04-01 10:15:53 +0530

    Update Swaptwonumbers.java

--- javacoreprograms/Swaptwonumbers.java
+++ javacoreprograms/Swaptwonumbers.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 public class Swaptwonumbers {
 
 	public static void main(String[] args) {
-		int a=10, b=20;
-
-		// TODO Auto-generated method stub
-		System.out.println("before swapping values are "+a+" "+b);
-int t= a;//10
-a=b;      //20
-t=b;       //10
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int a = sc.nextInt();
+		System.out.println("Enter the second number");
+		int b = sc.nextInt();
 
-System.out.println("After swapping values are "+a+" "+b);
+		int t = a;// 10
+		a = b; // 20
+		b = t; // 10
+//		
+//		a = a^b;
+//		b = a^b;
+//		a = a^b;
+//			
+		System.out.println("After swapping values are " + a + " " + b);
 
-
-//a = a+b;//30  a=a*b;    a = a^b;		          		        
-//b = a-b;//10  b=a/b     b = a^b;
-//a = a-b;//20  a=a/b     a = a^b;
+//a = a+b;//30  a=a*b;
+//b = a-b;//10  b=a/b
+//a = a-b;//20  a=a/b
 //System.out.println("a is " +a);
 //System.out.println("b is "+b);
+
 
 	
 	}
