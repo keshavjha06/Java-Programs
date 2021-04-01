319ee603b11d6a200bdb88b37d49f7c86bf8dc05
Keshav Jha <keshavjha0610@gmail.com>
2021-04-01 10:56:19 +0530

    Update SumOfArray.java

--- javacoreprograms/SumOfArray.java
+++ javacoreprograms/SumOfArray.java
@@ -3,7 +3,7 @@ package javacoreprograms;
 public class SumOfArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int a[] = {1,2,3,4,5}; //n-5  n-1=4
 		int sum=0;
 		
