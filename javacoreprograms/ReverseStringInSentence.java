90fc6f22601bccc172cb653c43391d650304ad32
Keshav Jha <keshavjha0610@gmail.com>
2021-05-16 19:43:43 +0530

    Update ReverseStringInSentence.java

--- javacoreprograms/ReverseStringInSentence.java
+++ javacoreprograms/ReverseStringInSentence.java
@@ -13,6 +13,10 @@ public class ReverseStringInSentence {
 		  
 		 for (int i = str1.length-1; i >= 0; i--) {   
 		 System.out.println(str1[i]); 
+		 if(i!= 0)
+		 {
+			System.out.print("*");
+		 }
 		 
 		 }
 
