31ccbc872566013f9d149ae47018f6c9b64332db
Keshav Jha <keshavjha0610@gmail.com>
2021-06-24 10:28:10 +0530

    Add files via upload

---
+++ javacoreprograms/PrintAlternateStar.java
@@ -0,0 +1,20 @@
+package javacoreprograms;
+
+public class PrintAlternateStar {
+
+	public static void main(String[] args) {
+		
+		String str = "Keshav";
+		for(int i=1;i<str.length();i++) {
+			//System.out.print(str);
+		
+			if(i%2==0)
+			{
+				System.out.println("*");
+			}
+			
+
+	}
+	}
+
+}
