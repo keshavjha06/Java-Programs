c0028f511c36c20fc43b2ae37c0689ed9e801971
keshav.jha <keshav.jha@medibuddy.in>
2022-08-16 14:21:58 +0530

    added missing character program

---
+++ javaprograms/MissingCharacter.java
@@ -0,0 +1,11 @@
+package javaprograms;
+
+import java.util.Arrays;
+
+public class MissingCharacter {
+    public static void main(String[] args) {
+        char[] c = {'a', 'b', 'c'};
+        c = new String(c).replace("b", "").toCharArray();
+        System.out.println("b is missing" + " " + Arrays.toString(c));
+    }
+}
