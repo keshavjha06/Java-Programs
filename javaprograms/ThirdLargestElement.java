09451cd810fe87afaa38698b511ec37b1c3869b1
keshav.jha <keshav.jha@medibuddy.in>
2022-08-17 16:43:02 +0530

    Added array programs

--- javaprograms/ThirdLargestElement.java
+++ javaprograms/ThirdLargestElement.java
@@ -5,12 +5,11 @@ public class ThirdLargestElement {
 
     public static void main(String[] args) {
 
-        int temp;
         int[] a = {10, 20, 25, 63, 96, 57};
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] > a[j]) {
-                    temp = a[i];
+                    int temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
                 }
