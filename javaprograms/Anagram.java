33b1e0707d3d6eac3c617b94b3e78ea8546b795e
keshav.jha <keshav.jha@medibuddy.in>
2022-08-20 13:10:09 +0530

    updated Anagram.java

--- javaprograms/Anagram.java
+++ javaprograms/Anagram.java
@@ -1,30 +1,23 @@
 package javaprograms;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Anagram {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first string");
-        String x = sc.nextLine();
-        System.out.println("Enter the second string");
-        String y = sc.nextLine();
-        x = x.replace(" ", "").toLowerCase();
-        y = y.replace(" ", "").toLowerCase();
+        System.out.println(anagramFinder("arc", "car"));
+    }
 
-        char[] a = x.toCharArray();
-        char[] b = y.toCharArray();
-
+    static boolean anagramFinder(String str, String s) {
+        if (str.length() != s.length())
+            return false;
+        str = str.replace(" ", "").toLowerCase();
+        s = s.replace(" ", "").toLowerCase();
+        char[] a = str.toCharArray();
+        char[] b = s.toCharArray();
         Arrays.sort(a);
         Arrays.sort(b);
-        boolean flag = Arrays.equals(a, b);
-        if (flag) {
-            System.out.println("Strings are anagram");
-        } else {
-            System.out.println("Strings are not anagram");
-        }
+        return Arrays.equals(a, b);
 
     }
 
