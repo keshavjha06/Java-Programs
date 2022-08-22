547e57d70ad78fa34146f4dd2431867aaf1682ea
keshav.jha <keshav.jha@medibuddy.in>
2022-08-22 13:01:42 +0530

    updated PrintAlternateStar

--- javaprograms/PrintAlternateStar.java
+++ javaprograms/PrintAlternateStar.java
@@ -5,15 +5,12 @@ public class PrintAlternateStar {
     public static void main(String[] args) {
 
         String str = "Keshav";
-        for (int i = 1; i < str.length(); i++) {
-            //System.out.print(str);
-
-            if (i % 2 == 0) {
-                System.out.println("*");
-            }
-
-
+        for (int i = 0; i <= str.length()-1; i++) {
+            if (i % 2 == 0)
+                System.out.print(str.charAt(i));
+            else
+                System.out.print("*");
         }
     }
-
 }
+
