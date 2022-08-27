18d54a55527ac694f8a8aa06b0bec6565b0d3a3e
keshav.jha <keshav.jha@medibuddy.in>
2022-08-27 22:16:13 +0530

    SwapTwoStrings.java

---
+++ javaprograms/SwapTwoStrings.java
@@ -0,0 +1,25 @@
+package javaprograms;
+
+public class SwapTwoStrings {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "World";
+
+        // Print String before swapping
+        System.out.println("Strings before swap: a = " + a + " and b = " + b);
+
+        // append 2nd string to 1st
+        a = a + b;
+
+        // store initial string a in string b
+        b = a.substring(0, a.length() - b.length());
+
+        // store initial string b in string a
+        a = a.substring(b.length());
+
+        // print String after swapping
+        System.out.println("Strings after swap: a = " + a + " and b = " + b);
+    }
+
+
+}
