b0558539dcafa075ca99b51cda9715e3d449bd74
keshav.jha <keshav.jha@medibuddy.in>
2022-08-28 12:07:51 +0530

    Added SortStringSequence.java

---
+++ javaprograms/SortStringSequence.java
@@ -0,0 +1,31 @@
+package javaprograms;
+import java.util.*;
+
+public class SortStringSequence {
+    public static void main(String[] args) {
+        String str = "azcazdado";
+        LinkedHashMap<Character,Integer> maxCount = new LinkedHashMap<>();
+        char[] chars = str.toCharArray();
+        for(Character character : chars){
+            if(maxCount.containsKey(character)) {
+                int count = maxCount.get(character);
+                        count++;
+                maxCount.put(character, count);
+            }
+            else {
+                maxCount.put(character,1);
+            }
+        }
+        StringBuilder newString = new StringBuilder();
+        for(Map.Entry<Character,Integer> entry : maxCount.entrySet()) {
+            int itr = entry.getValue();
+            char ch = entry.getKey();
+            for(int i= 0;i<itr;i++){
+                newString.append((ch));
+            }
+         //   newString.append(String.valueOf(ch).repeat(Math.max(0,itr)));
+            //repeat will work with jdk 11
+        }
+        System.out.println(newString);
+    }
+}
