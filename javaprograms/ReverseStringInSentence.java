ef8d0f27ed2a46382c2696a8149d039d5907cf80
keshav.jha <keshav.jha@medibuddy.in>
2022-09-10 20:57:05 +0530

    Modified ReverseStringInSentence.java

--- javaprograms/ReverseStringInSentence.java
+++ javaprograms/ReverseStringInSentence.java
@@ -12,7 +12,7 @@ public class ReverseStringInSentence {
         String[] str1 = str.split(" ");
 
         for (int i = str1.length - 1; i >= 0; i--) {
-            System.out.println(str1[i]);
+            System.out.print(str1[i]);
             if (i != 0) {
                 System.out.print("*");
             }
