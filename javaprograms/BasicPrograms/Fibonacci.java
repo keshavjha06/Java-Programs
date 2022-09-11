d7213667f3da0cdfdcf1a6dcc83749a48f404630
keshav.jha <keshav.jha@medibuddy.in>
2022-09-11 12:01:36 +0530

    Updated Fibonacci.java

--- javaprograms/BasicPrograms/Fibonacci.java
+++ javaprograms/BasicPrograms/Fibonacci.java
@@ -12,11 +12,10 @@ public class Fibonacci {
         int num = sc.nextInt();
         int f1, f2 = 0, f3 = 1;
         for (int i = 1; i <= num; i++) {
-            System.out.println(f3);
             f1 = f2;
             f2 = f3;
             f3 = f1 + f2;
-
+            System.out.print(f3+" ");
         }
     }
 }
