b485dd4d6955daa931430b33667a5f551ec8ee5a
Keshav Jha <keshavjha0610@gmail.com>
2026-06-04 16:16:37 +0530

    refactor: change removeDuplicates to static method and remove unnecessary class instantiation

--- javaprograms/strings/RemoveDuplicateCharacters.java
+++ javaprograms/strings/RemoveDuplicateCharacters.java
@@ -6,17 +6,17 @@ public class RemoveDuplicateCharacters {
 
     public static void main(String[] args) {
         String str = "Keshav Jha";
-        RemoveDuplicateCharacters r = new RemoveDuplicateCharacters();
-        r.removeDuplicates(str);
+        removeDuplicates(str);
 
     }
 
-    void removeDuplicates(String str) {
+    static void removeDuplicates(String str) {
         LinkedHashSet<Character> lhs = new LinkedHashSet<>();
         for (int i = 0; i < str.length(); i++) {
             lhs.add(str.charAt(i));
         }
+        // System.out.println(lhs.toString().replaceAll("[, ]", ""));
         for (Character ch : lhs)
-            System.out.print(ch);
+        System.out.print(ch);
     }
 }
