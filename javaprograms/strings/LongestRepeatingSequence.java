2c376651b726d27a4acfe851328c2ab9d8d36d44
Keshav Jha <keshavjha0610@gmail.com>
2026-06-07 23:32:29 +0530

    refactor: optimize longest repeating sequence algorithm and improve code style in LongestSubstring

--- javaprograms/strings/LongestRepeatingSequence.java
+++ javaprograms/strings/LongestRepeatingSequence.java
@@ -1,29 +1,28 @@
 package javaprograms.strings;
 
-public class LongestRepeatingSequence {
-    //Checks for the largest common prefix
-    public static String lcp(String s, String t) {
-        int n = Math.min(s.length(), t.length());
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) != t.charAt(i)) {
-                return s.substring(0, i);
-            }
-        }
-        return s.substring(0, n);
-    }
+import java.util.HashMap;
+import java.util.Map;
 
+public class LongestRepeatingSequence {
     public static void main(String[] args) {
         String str = "acbdfghybdf";
         String lrs = "";
         int n = str.length();
+
+        Map<String, Integer> substringCounts = new HashMap<>();
+        // Generate all possible substrings and count their occurrences
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                //Checks for the largest common factors in every substring
-                String x = lcp(str.substring(i, n), str.substring(j, n));
-                //If the current prefix is greater than previous one
-                //then it takes the current one as longest repeating sequence
-                if (x.length() > lrs.length())
-                    lrs = x;
+            for (int j = i + 1; j <= n; j++) {
+                String sub = str.substring(i, j);
+                substringCounts.put(sub, substringCounts.getOrDefault(sub, 0) + 1);
+            }
+        }
+        // Find the longest substring that appears at least twice (count > 1)
+        for (Map.Entry<String, Integer> entry : substringCounts.entrySet()) {
+            String sub = entry.getKey();
+            int count = entry.getValue();
+            if (count > 1 && sub.length() > lrs.length()) {
+                lrs = sub;
             }
         }
         System.out.println("Longest repeating sequence: " + lrs);
